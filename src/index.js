5700b86b7f8f4551737b6e8571397c40bad36247
Tyler Garlick <tjgarlick@gmail.com>
2018-02-26 20:15:28 -0700

    Solutions

--- src/index.js
+++ src/index.js
@@ -1,35 +1,48 @@
 import Senators from './data/senators'
 
 export const republicans = () => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.party === 'Republican')
 }
 
 export const democrats = () => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.party === 'Democrat')
 }
 
 export const independents = () => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.party === 'Independent')
 }
 
 export const males = () => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.person.gender === 'male')
 }
 
 export const females = () => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.person.gender === 'female')
 }
 
 export const byState = (state = 'UT') => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator => senator.state === state)
 }
 
 export const mapping = () => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return Senators.map(senator => ({
+    firstName: senator.person.firstname,
+    lastName: senator.person.lastname,
+    party: senator.party,
+    gender: senator.person.gender
+  }))
 }
 
 export const birthplaceSeniorSenator = () => {
-  return REPLACE_ME_WITH_CODE
+  // return REPLACE_ME_WITH_CODE
+  return byState('UT').filter(senator => senator.senator_rank === 'senior').reduce((prev, current) => current)
 }
 
 
